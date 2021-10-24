0fb0af888da014db5533825a0663c5515f67e691
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2021-10-24 23:12:09 +0530

    Ignore Escape Entity Error

--- components/main.js
+++ components/main.js
@@ -5,7 +5,7 @@ export default function Main() {
   return (
     <main className="pt-14 flex flex-col-reverse md:flex-row md:items-center justify-center h-screen dark:bg-primary">
       <section className="md:w-1/2 flex flex-col justify-start md:justify-center items-center md:items-end my-2">
-        <h5 className="px-1 md:text-2xl lg:text-5xl text-gray-700 dark:text-gray-400 md:w-3/5 mx-4 text-wrap text-center md:text-left">
+        <h5 className="px-1 md:text-2xl lg:text-5xl sub-text md:w-3/5 mx-4 text-wrap text-center md:text-left">
           Hobbyist programmer by night,<br/>
           By day, just your regular college student.
         </h5>
