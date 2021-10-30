509d135a88da58662663de9d83fc3c6128a70fdc
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2021-10-31 01:29:28 +0530

    discarded fullpage.js because of problems with slides in mobile view

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
 import Head from 'next/head';
-import React from "react";
-
-// components
 import Navbar from '../components/navbar';
 import Main from '../components/main';
 import About from '../components/about';
-
-// fullpage
-import ReactFullpage from '@fullpage/react-fullpage';
 
 export default function Home() {
   return (
@@ -17,17 +11,11 @@ export default function Home() {
         <meta name="description" content="Pranshu Jha's Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Navbar />
-      <ReactFullpage
-          navigation
-          render={comp => (
-              <ReactFullpage.Wrapper>
-                <div className="section"><Main /></div>
-                <div className="section"><About /></div>
-              </ReactFullpage.Wrapper>
-            )
-          }
-      />
+      <Main />
+      <About />
+
     </main>
   )
 }
