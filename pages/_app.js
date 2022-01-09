3a824eeb100bd87c0e0634679dca16d8716328ba
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-01-09 16:57:57 +0530

    removed analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,8 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
-// ANALYTICS
-import * as ga from '../lib/analytics';
-
 // STYLES
 import '../styles/tailwind.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
   return <Component {...pageProps} />
 }
 
