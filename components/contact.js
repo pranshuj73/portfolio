202b5f1ff9def73fc04b4631956d8393dd3d3bde
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-04-02 17:38:07 +0530

    Update contact.js

--- components/contact.js
+++ components/contact.js
@@ -15,7 +15,7 @@ export default function Contact() {
             <a className="pill m-2" href="https://twitter.com/pranshuj73" target="_blank" rel="noopener noreferrer"><IoLogoTwitter className="pill-icon" size='1.5em'/>Twitter</a>
           </div>
         </div>
-        <a className="pill m-2" href="https://pranshu.codes/" target="_blank" rel="noopener noreferrer"><SiHashnode className="pill-icon" size='1.5em'/>My Blog</a>
+        <a className="pill m-2" href="https://blog.pranshu.codes/" target="_blank" rel="noopener noreferrer"><SiHashnode className="pill-icon" size='1.5em'/>My Blog</a>
       </section>
     </section>
   )
