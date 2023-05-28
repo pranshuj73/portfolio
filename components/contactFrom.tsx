11182b6a001d36870839e1c5456496ce459f2e87
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2023-05-28 17:05:48 +0530

    refactor(header,-contact,-app): centered page headings, updated header ui for smaller screens

--- components/contactFrom.tsx
+++ components/contactFrom.tsx
@@ -20,13 +20,13 @@ function Social(props: PropsWithChildren<SocialProps>) {
 
 export default function ContactForm() {
   return (
-    <section className="container px-10 py-24 mx-auto flex flex-col items-center text-gray-400 -mt-20">
-      <div className="flex flex-col w-full mb-20">
+    <section className="px-10 py-24 mx-auto flex flex-col items-center text-gray-400">
+      <div className="flex flex-col md:w-full">
         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">reach out to me.</h1>
         <div className="h-1 w-20 bg-gray-500 rounded" />
+        <h1 className="text-left mt-3">email: pranshuj9801@gmail.com</h1>
       </div>
-      <h1 className="text-2xl text-white text-center">you can reach out to me at my socials.</h1>
-      <div className="flex flex-row flex-wrap items-center justify-center mt-6">
+      <div className="flex flex-row flex-wrap items-center justify-center mt-4 -mx-5">
         <Social name="twitter" colorClass="text-blue-400" link="https://twitter.com/pranshuj73">
           <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
         </Social>
--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,12 @@
 export default function Header() {
-  const linkStyle = 'mr-5 text-gray-400 hover:text-gray-200 transition-all duration-300 ease-in cursor-pointer'
+  const linkStyle = 'mx-2 text-gray-400 hover:text-gray-200 transition-all duration-300 ease-in cursor-pointer'
 
   return (
-    <header className="body-font w-full mx-auto flex p-5 flex-row items-baseline justify-between">
+    <header className="body-font w-full flex p-5 flex-row items-baseline justify-between">
       <a className="flex title-font font-medium items-center mb-4 md:mb-0" href="/">
-        <span className="ml-3 text-xl">pranshu jha.</span>
+        <span className="text-xl">pranshu jha.</span>
       </a>
-      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <nav className="md:ml-auto text-sm md:text-base flex flex-wrap items-center justify-center -mr-2">
         <a className={linkStyle} href="/about">about</a>
         <a className={linkStyle} href="/projects">projects</a>
         <a className={linkStyle} href="/contact">contact</a>
