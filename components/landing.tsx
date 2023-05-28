7a50e75cf7922883bd473454981b46ed1b7351ee
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2023-05-28 17:19:36 +0530

    fix whatever i messed up

--- components/landing.tsx
+++ components/landing.tsx
@@ -2,7 +2,7 @@ import Skills from "./skills";
 
 export default function Landing() {
   return (
-    <section className="flex flex-col p-10 items-center justify-center text-center">
+    <section className="flex flex-col p-10 items-center justify-center text-center mt-12">
       <h1 className='text-3xl md:text-5xl'>hey, i'm pranshu.</h1>
       <p className='mt-4 text-lg md:text-xl  text-gray-400'>full-stack developer & open-source enthusiast.</p>
       <Skills />
