f14d808153c68408c3b04beecfe71759731c97e4
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2023-10-19 00:44:49 +0530

    feat(projects): add in-progress indicator for incomplete projects, added links

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,6 +1,6 @@
-export default function Section(props: { children: React.ReactNode }) {
+export default function Section(props: { children: React.ReactNode, className?: string }) {
   return (
-    <section className='min-h-screen max-w-full md:max-w-screen-2xl flex flex-col items-center justify-center relative p-6 xs:p-10'>
+    <section className={'min-h-screen w-full max-w-full md:max-w-screen-2xl flex flex-col items-center justify-center relative p-6 xs:p-10 ' + props.className}>
       {props.children}
     </section>
   )
--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 import { Link } from "@nextui-org/link";
 
 
-interface projectData{id: number, title: string, description: string, tags: string};
+interface projectData{id: number, title: string, description: string, tags: string, link?: string};
 
-const Project = ({id, title, description, tags}: projectData) => {
+const Project = ({id, title, description, tags, link}: projectData) => {
   return (
     <div key={id.toString()} className="flex flex-col my-6">
       <div className="w-full h-auto relative">
@@ -20,14 +20,25 @@ const Project = ({id, title, description, tags}: projectData) => {
           className="w-full object-cover h-auto rounded-2xl shadow-md border-1 border-black/10"
           src={`/projects/${id}.jpg`}
         />
-        <Link
-          as={Button}
-          className="absolute bottom-3 right-3 z-10 font-medium"
-          size="sm" radius="lg" color="foreground" variant="solid"
-          isExternal showAnchorIcon
-        >
-          <span className="hidden md:block">Link</span>
-        </Link>
+        { link ? (
+          <Button
+            as={Link}
+            href={link}
+            className="absolute bottom-3 right-3 z-10 font-medium"
+            size="md" radius="md" color="default" variant="solid"
+            isExternal showAnchorIcon
+          >
+            <span className="hidden md:block">Link</span>
+          </Button>
+        ) : (
+          <Button
+            className="absolute bottom-3 right-3 z-10 font-medium"
+            size="md" radius="md" color="warning" variant="solid"
+            disabled
+          >
+            <span className="text-xs md:text-[14px]">In Progress</span>
+          </Button>
+        ) }
       </div>
       <div className="flex flex-col xs:flex-row justify-between items-start xs:items-center w-full mb-2 mt-6">
         <h4 className="text-text font-medium text-2xl my-1">{title}</h4>
@@ -42,11 +53,13 @@ export default function Projects() {
   const data: projectData[] = [
     {
       id: 1, tags: "NextJS", title: "Dystopian",
-      description: "Dystopian is a gamified learning platform that allows students to learn programming easily through bite-sized lessons built into the plot of the game. Students can also compete with each other in the leaderboard."
+      description: "Dystopian is a gamified learning platform that allows students to learn programming easily through bite-sized lessons built into the plot of the game. Students can also compete with each other in the leaderboard.",
+      link: "https://github.com/Razor-Code/dystopian"
     },
     { 
       id: 2, tags: "Blockchain", title: "XDC Health",
-      description: "Built for PLI Blockathon '22, XDC Health is an Electronic Healthcare Management System built upon XDC Blockchain allowing healthcare recorrds to be stored on the blockchain."
+      description: "Built for PLI Blockathon '22, XDC Health is an Electronic Healthcare Management System built upon XDC Blockchain allowing healthcare recorrds to be stored on the blockchain.",
+      link: "https://github.com/Razor-Code/xdc-health-revise"
     },
     {
       id: 3, tags: "NextJS, TailwindCSS", title: "Tranquil",
@@ -55,10 +68,10 @@ export default function Projects() {
   ]
   return (
     <Section>
-      <h1 className="text-2xl mt-24 mb-10">Projects</h1>
+      <h1 className="typing-demo text-4xl mt-24 mb-10">&gt; Projects</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12 my-10">
         {data.map((project) => <Project {...project} />)}
-        <Project id={0} title="More Projects" description="Check out more of my projects on GitHub." tags="GitHub" />
+        <Project id={0} title="More Projects" description="Check out more of my projects on GitHub." link="https://github.com/pranshuj73" tags="GitHub" />
       </div>
     </Section>
   )
