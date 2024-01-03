ddddbf234a646eb0bfa28fcedde420eb069aa2d9
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-01-03 23:15:36 +0530

    build(nextConfig): add support for hashnode blog on /blog path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://pranshus-blog.vercel.app/blog",
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://pranshus-blog.vercel.app/blog/:path*",
+      }
+    ];
+  }
+}
 
 module.exports = nextConfig
