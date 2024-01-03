c052cfd09c638b2b0598100f9f711e0e742c40af
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-01-04 02:01:09 +0530

    style(About): justify text

--- sections/About.tsx
+++ sections/About.tsx
@@ -8,7 +8,7 @@ export default function About() {
     <section id="about" className='scroll-mt-8 min-h-screen max-w-full md:max-w-screen-2xl grid grid-cols-1 md:grid-cols-2 gap-12 p-6 xs:p-10 my-8'>
       <ParallaxImage src="/about.jpg" alt="Pranshu Jha" className="rounded-lg grayscale hover:grayscale-0 duration-700 delay-150 transition-all ease-soft-spring" />
 
-      <div className="m-4 max-w-2xl flex flex-col justify-center">
+      <div className="m-4 max-w-2xl flex flex-col justify-center text-justify">
         <h1 className="text-4xl font-semibold mb-3">Hello World!</h1>
         <p className="text-text my-3">This is Pranshu Jha. I am an undergrad currently pursuing Bachelors in Engineering, majoring in Computer Science.</p>
         <p className="text-text my-3">As a Full-Stack Developer, I bridge the gap between captivating front-end designs and robust back-end functionality, delivering exceptional user experiences with precision and finesse.</p>
