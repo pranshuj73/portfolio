30b23507394b1ee777960634daca46fdb52defc7
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-06-16 21:47:11 +0530

    welcome to astro

--- components/Scaffold.tsx
+++
@@ -1,7 +0,0 @@
-export default function Scaffold(props: { children: React.ReactNode }) {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center -mt-[4rem]">
-      {props.children}
-    </main>
-  )
-}
