9971537b7d49412046ba5e825770b86457986f66
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-07-09 23:11:40 +0530

    static bundler

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false
   }), mdx()],
-  output: "server",
+  output: "static",
   adapter: vercel()
 });
