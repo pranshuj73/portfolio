672eb870abf0b7a1e7143ba21016c67ba72c40bf
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-07-09 23:02:17 +0530

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), mdx()]
+  }), mdx()],
+  output: "server",
+  adapter: vercel()
 });
